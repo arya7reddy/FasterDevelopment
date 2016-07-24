3838a6ffa25cb470a4d8130bbe7bc97b19faaacc
Bharath Reddy <arya7reddy@gmail.com>
2016-07-23 19:46:51 -0700

    gulp file with a default task to compile less files to css files in the assets directory. less files with _ prefix are ignored.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var watchLess = require('gulp-watch-less2');
 var less = require('gulp-less');
-var lessGlobs = 'less/main-*.less';
+var lessGlobs = 'assets/less/main.less';
+
 gulp.task('default', function () {
   return gulp.src(lessGlobs)
     .pipe(watchLess(lessGlobs, {verbose: true}))
     .pipe(less())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("assets/css/"));
 });
